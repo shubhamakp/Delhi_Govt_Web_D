d7040337e33f26c93df3f5682ede294e4b21620d
unknown <shubhamakp@gmail.com>
2021-01-15 12:30:56 +0530

    lecture 8

---
+++ lecture8/index.js
@@ -0,0 +1,30 @@
+// to check whether a number is odd or even 
+
+var x = 12;  // variable declaration and initialization
+
+if(x%2==0){ //condition 1 checked
+    let z = 12;
+    var q = 13;
+    console.log(x + " is Even");
+}else {
+    console.log(x + " is Odd");
+}
+
+// console.log(x);
+// console.log(z);
+// console.log(q);
+
+
+
+var marks = 39;
+
+if(marks >= 80){
+    console.log("A grade");
+}else if(marks >= 60 && marks < 80){
+    console.log("B grade");
+}else if(marks >= 40 && marks < 60){
+    console.log("C grade");
+}else{
+    console.log("F grade");
+}
+
