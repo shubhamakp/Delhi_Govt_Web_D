92cb3df04a6c6df446f693a9d235c47f68620756
unknown <shubhamakp@gmail.com>
2021-01-15 15:13:18 +0530

    lecture 8

--- lecture8/index.js
+++ lecture8/index.js
@@ -2,11 +2,11 @@
 
 var x = 12;  // variable declaration and initialization
 
-if(x%2==0){ //condition 1 checked
+if (x % 2 == 0) { //condition 1 checked
     let z = 12;
     var q = 13;
     console.log(x + " is Even");
-}else {
+} else {
     console.log(x + " is Odd");
 }
 
@@ -19,13 +19,13 @@ if(x%2==0){ //condition 1 checked
 
 var marks = 99;
 
-if(marks >= 80){
+if (marks >= 80) {
     console.log("A grade");
-}else if(marks >= 60 && marks < 80){
+} else if (marks >= 60 && marks < 80) {
     console.log("B grade");
-}else if(marks >= 40 && marks < 60){
+} else if (marks >= 40 && marks < 60) {
     console.log("C grade");
-}else{
+} else {
     console.log("F grade");
 }
 
@@ -49,8 +49,56 @@ if(marks >= 80){
 
 //  do while loop
 
-var q = 9;
-do{
-    console.log("hello everyone "+q);
-    q++;
-}while(q<=10);
+// var q = 9;
+// do{
+//     console.log("hello everyone "+q);
+//     q--;
+//     if(q<0){
+//         break;
+//     }
+// }while(q<=10);
+
+
+
+// to check whether a number is prime or not 
+
+// var number = 2;
+// var isPrime = true;
+
+// for(var i = 2; i < number ; i++){
+//     if(number % i == 0){
+//         isPrime = false;
+//         break;
+//     }
+// }
+
+// if(isPrime==true && number != 1){
+//     console.log(number + " is prime");
+// }
+// else {
+//     console.log(number + " is not prime");
+// }
+
+
+// Another method 
+
+var number = 1;
+var isPrime = true;
+
+if (number == 1) {
+    console.log(number + " not prime");
+} else {
+    for (var i = 2; i < number; i++) {
+        if (number % i == 0) {
+            isPrime = false;
+            break;
+        }
+    }
+
+    if (isPrime == true) {
+        console.log(number + " is prime");
+    }
+    else {
+        console.log(number + " is not prime");
+    }
+}
