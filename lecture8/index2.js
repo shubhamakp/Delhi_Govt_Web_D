ce8a3e92b481fd81059d949858343ad4c7456660
unknown <shubhamakp@gmail.com>
2021-01-15 16:01:20 +0530

    lecture 8

--- lecture8/index2.js
+++ lecture8/index2.js
@@ -13,8 +13,29 @@ sum=sum*2;
 console.log(sum);
 
 
+// Arrow functions
+
 const addd = () =>{
     return 5+8;
 }
 
 console.log(addd());
+
+// Arrays
+
+// creation
+
+// var arrayname = [item 1, item 2,item 3];
+
+var fruits = ["orange","mango","banana" , "kiwi"];
+console.log("length of array is " +fruits.length);
+
+// accessing array elements
+
+console.log(fruits[2])
+
+fruits.push("grapes");
+console.log(fruits);
+
+fruits.pop();
+console.log(fruits.indexOf("mango"));
