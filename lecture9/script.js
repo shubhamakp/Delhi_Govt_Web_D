ce41a98479a849d22749b42ac9c7096295660a3c
unknown <shubhamakp@gmail.com>
2021-01-18 12:32:55 +0530

    lecture 9

--- lecture9/script.js
+++ lecture9/script.js
@@ -38,3 +38,26 @@ console.log(fruits);
 // reversing the array
 fruits.reverse();
 console.log(fruits);
+
+
+
+// STRINGS FUNCTIONS
+
+var example= "Web development";
+console.log(example);
+
+let slicedExample = example.slice(0,3);
+console.log(slicedExample);
+
+console.log(example.slice(4,example.length))
+console.log(example.slice(4));
+
+// substr
+
+console.log(example.substr(0,3));
+console.log(example.substr(4,11));
+
+// toUpperCase & toLowerCase
+
+console.log(example.toUpperCase());
+console.log(example.toLowerCase())
