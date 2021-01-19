db08b630ec1b617b83fe25e9ec27c2bbd063449c
unknown <shubhamakp@gmail.com>
2021-01-19 11:49:17 +0530

    lecture 10

--- lecture10/index.js
+++ lecture10/index.js
@@ -18,3 +18,21 @@ person.city = "Delhi";
 person.Age = 36;
 console.log(person)
 console.log(person.x.food);
+
+
+
+// Prototype
+
+let p = {
+    a : 10
+}
+
+let q = Object.create(p);  // it will use p as its prototype/blue print.
+q.b = 15;
+
+// console.log(p);
+console.log( " p's a :" + p.a)
+// console.log(q);
+console.log("q's b :" + q.b)
+console.log("q's a : " +q.a)
+
