ce0ed97e577c40b1f38962b28c60ee7bc203666d
unknown <shubhamakp@gmail.com>
2021-01-19 12:40:05 +0530

    lecture 10

--- lecture10/constructor.js
+++ lecture10/constructor.js
@@ -8,10 +8,17 @@ function Person(first,last,age){
     }
 }
 
-var person1 = new Person("Monu","Kumar",46);
 
-console.log(person1.greet())
+
+var person1 = new Person("Monu","Kumar",46);
+// for in loop
+
+for(x in person1){
+    console.log(x + " : " + person1[x]);
+}
+
+// console.log(person1.greet())
 var person2 = new Person("Suresh","kumar",56);
-console.log(person1);
-console.log(person1.firstName)
+// console.log(person1);
+// console.log(person1.firstName)
 
