2800ad78639477f3c5e73ae11004ea55abb70657
unknown <shubhamakp@gmail.com>
2021-01-19 15:00:18 +0530

    lecture 10

--- lecture10/oops.js
+++ lecture10/oops.js
@@ -39,3 +39,17 @@ console.log(book2)
 console.log(Book.BookStore())
 // console.log(book2.getSummary())
 // console.log(book2.getAge())
+
+//Magzine class is inheriting the features of Book class
+class Magzine extends Book {
+    constructor(title,author,year,month){
+        super(title,author,year);
+        this.month=month;
+    }
+}
+
+var mag1 = new Magzine("Godaan","Munshi PremChand",1925,"February");
+var mag2 = new Magzine("2 States","Chetan Bhagat",2012,"November");
+console.log(mag2);
+
+console.log(mag1.getSummary())
