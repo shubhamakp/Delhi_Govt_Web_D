ec6ac688b2a20da0ba4eda83f86e30d3516f48eb
unknown <shubhamakp@gmail.com>
2021-01-19 15:23:47 +0530

    lecture 10

---
+++ lecture10/async.js
@@ -0,0 +1,21 @@
+// setTimeout
+setTimeout(()=>{
+    console.log("it will print after 5 sec");
+} , 5000)
+
+
+// setInterval
+var count = 0;
+setInterval(()=>{
+    count++;
+    console.log("it will print after every 1 sec " + count);
+} , 1000)
+
+var count = 0;
+let intervalId  = setInterval(()=>{
+    count++;
+    console.log("it will print after every 1 sec " + count);
+    if(count>=5){
+        clearInterval(intervalId)
+    }
+} , 1000)
